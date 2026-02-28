b79e7dbe79da0e7397a9cff7ff7c41b0ecb88767
kirhen <XNhtOqlEqeiONqYMJeXN@gmx.de>
2026-02-28 10:58:31 +0100

    added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { initMQTT } from './mqttHandler.js'
 import restHandler from './restHandler.js'
 import { connectToDatabase } from './db.js'
 import 'dotenv/config'
 
 const app = express()
+app.use(cors()) // allow any origin
 app.use(express.json())
 
 const broker = initMQTT()
